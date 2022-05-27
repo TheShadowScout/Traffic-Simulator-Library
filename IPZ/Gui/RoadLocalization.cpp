2718412a11b748affcebdfde553411f9c7833340
Gabor Łukjaniuk <lg46585@zut.edu.pl>
2022-05-27 14:01:08 +0200

    code fix

--- IPZ/Gui/RoadLocalization.cpp
+++ IPZ/Gui/RoadLocalization.cpp
@@ -16,9 +16,11 @@ void RoadLocalization::prepShapes(float cellSize, std::vector<sf::RectangleShape
     rotateShape(cellSize, &roadRectangle, road->getLength(), road->getHeight(), 0, 0, moveDirection);
     roadRectangle.setFillColor(sf::Color(211, 211, 211));
     shapes->push_back(roadRectangle);
+    int roadLength = road->getLength();
+    int roadHeight = road->getHeight();
     for (int i = 0; i < road->getHeight(); i++) {
         for (int j = 0; j < road->getLength(); j++) {
-            createCellShapes(cellSize, shapes, road->getLanes()[i][j], road->getLength(), road->getHeight(), j, i, moveDirection);
+            createCellShapes(cellSize, shapes, road->getLanes()[i][j], roadLength, roadHeight, j, i, moveDirection);
         }
     }
 }
